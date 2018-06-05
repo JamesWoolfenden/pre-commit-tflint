0f24550ee590b78006bfc1cd0ed093695a428a1f
James Woolfenden <James.Woolfenden@gmail.com>
2018-06-05 13:56:44 +0100

    file with path

--- bin/tf_lint.sh
+++ bin/tf_lint.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-declare -a paths
-index=0
-
 for file_with_path in "$@"; do
-  paths[index]=$(dirname "$file_with_path")
-  let "index+=1"
-done
-
-for path_uniq in $(echo "${paths[*]}" | tr ' ' '\n' | sort -u); do
-  pushd "$path_uniq" > /dev/null
-  tflint 
-  popd > /dev/null
+  tflint "$file_with_path"
 done
